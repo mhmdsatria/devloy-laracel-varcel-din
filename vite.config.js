e5b5ed174c30b6bd4b6334ea1cd2324905a983a7
muhamad satria <114433514+mhmdsatria@users.noreply.github.com>
2025-06-23 11:28:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-    },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
 });
